007442d0451b6fe2a006fcf9e5ec2301c7a03b81
AlekseiKachkin <lex6164665@yandex.ru>
2022-05-10 23:13:13 +0300

    feat add styly for Virtual Keyboard

--- src/index.js
+++ src/index.js
@@ -2,34 +2,38 @@ const keyboardCharEn = [
                          ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'], 
                          ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'DEL'],
                          ['Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'",  'Enter'],
-                         ['Shift', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'Up', 'Shift'],
-                         ['Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', 'Left', 'Down', 'Right']
+                         ['⇧ Shift', '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '▲', '⇧'],
+                         ['Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '⮜', '▼', '⮞']
                        ];
 
 const keyboardCharEnBig = [ 
                             ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace'], 
                             ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', 'DEL'],
                             ['Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"',  'Enter'],
-                            ['Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Up', 'Shift'],
-                            ['Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', 'Left', 'Down', 'Right']
+                            ['⇧ Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '▲', '⇧'],
+                            ['Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '⮜', '▼', '⮞']
                           ];
 
 const keyboardCharRu = [ 
                           ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'], 
                           ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', 'DEL'],
                           ['Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', "э",  'Enter'],
-                          ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'Up', 'Shift'],
-                          ['Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', 'Left', 'Down', 'Right']
+                          ['⇧ Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '▲', '⇧'],
+                          ['Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '⮜', '▼', '⮞']
                         ];
  
 const keyboardCharRuBig = [ 
                              ['Ё', '!', '"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace'], 
                              ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', 'DEL'],
                              ['Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э',  'Enter'],
-                             ['Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', 'Up', 'Shift'],
-                             ['Ctrl', 'Win', 'Alt', '', 'Alt', 'Ctrl', 'Left', 'Down', 'Right']
+                             ['⇧ Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '▲', '⇧'],
+                             ['Ctrl', 'Win', 'Alt', ' ', 'Alt', 'Ctrl', '⮜', '▼', '⮞']
                           ];
 import './style.scss';
+
+const controls = ['`', '~', 'Backspace', 'Tab', 'DEL','Caps Lock', 'Enter', '⇧ Shift', '▲', 'Ctrl' , 'Win', 'Alt', ' ', '⮜', '▼', '⮞', '⇧'];
+
+let currentLang = 'eng';
 
 const WRAPPER = document.createElement('div');
 const TEXTAREA = document.createElement('textarea');
@@ -42,17 +46,61 @@ WRAPPER.classList.add('wrapper');
 TEXTAREA.classList.add('text-area');
 KEYBOARD.classList.add('buttons-wrap');
 
-function addRow(i){
+function addRow(lang, i){
+  let letterArray;
+  if (lang === 'eng') {
+    letterArray = keyboardCharEn
+  } else {
+    letterArray = keyboardCharRu
+  }
   const ROW = document.createElement('div');
   KEYBOARD.append(ROW);
   ROW.classList.add('buttons-row');
-  keyboardCharEn[i].forEach((char) => {
+  letterArray[i].forEach((char) => {
     const BTN = document.createElement('div');
     ROW.append(BTN);
     BTN.innerText = char;
+    BTN.classList.add('button');
+    if (controls.includes(char)) {
+      BTN.classList.add('button-manage');
+    } else {
+      BTN.classList.add('button-text');
+    }
+    switch (char) {
+      case '`':
+      case '~':
+      case 'DEL':
+      case 'Alt':
+      case 'Win':
+      case '▲':
+      case '⮜':
+      case '▼':
+      case '⮞':
+      case '⇧':
+        BTN.classList.add('tilda');
+        break;
+      case 'Backspace':
+      case 'Caps Lock':
+      case '⇧ Shift':
+        BTN.classList.add('backspace');
+        break;
+      case 'Tab':
+        BTN.classList.add('tab');
+        break;
+      case 'Enter':
+        BTN.classList.add('enter');
+        break;
+      case 'Ctrl':
+        BTN.classList.add('ctrl');
+        break;
+      case ' ':
+          BTN.classList.add('space');
+          break;
+
+    }    
   })
 }
 
 for(let i = 0; i < 5; ++i) {
-  addRow(i);
+  addRow(currentLang, i);
 }
